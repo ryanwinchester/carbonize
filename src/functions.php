9386f6408fc6ddf0d31e03b1301af7731cd28c19
Ryan Winchester <code@ryanwinchester.ca>
2017-10-14 14:09:15 -0700

    remove unnecessary functions

--- src/functions.php
+++ src/functions.php
@@ -20,38 +20,12 @@ if (! function_exists('carbonize')) {
                 return Carbon::instance($time);
             case $time instanceof DateTimeImmutable:
                 return Carbon::instance(new DateTime($time->format(DateTime::ATOM)));
-            case is_timestamp($time):
+            case is_numeric($time) && (string) (int) $time === (string) $time:
                 return Carbon::createFromTimestamp((int) $time, $tz);
-            case is_datetime_string($time):
+            case is_string($time) && strtotime($time) !== false:
                 return Carbon::parse($time, $tz);
             default:
                 throw new InvalidArgumentException("I don't know what to do with this.");
         }
-    }
-}
-
-if (! function_exists('is_datetime_string')) {
-    /**
-     * Check if it might be a datetime string.
-     *
-     * @param mixed $time
-     * @return bool
-     */
-    function is_datetime_string($time)
-    {
-        return is_string($time) && strtotime($time) !== false;
-    }
-}
-
-if (! function_exists('is_timestamp')) {
-    /**
-     * Check if it might be a valid timestamp.
-     *
-     * @param mixed $time
-     * @return bool
-     */
-    function is_timestamp($time)
-    {
-        return is_numeric($time) && (string) (int) $time === (string) $time;
     }
 }
