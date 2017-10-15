81d49573a3e1c9147c8ff965b3ef4dab84ffc0de
Ryan Winchester <code@ryanwinchester.ca>
2017-10-14 22:29:35 -0700

    let's OOP all the things for fun

---
+++ src/Carbonize.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Carbonize;
+
+use Carbon\Carbon;
+use DateTimeInterface;
+use DateTimeZone;
+use InvalidArgumentException;
+
+class Carbonize
+{
+    /**
+     * @param mixed $datetime
+     * @param DateTimeZone|string|null $timezone
+     * @return Carbon
+     * @throws InvalidArgumentException
+     */
+    public static function toCarbon($datetime = null, $timezone = null)
+    {
+        $carbonize = new static;
+
+        switch (true) {
+            case is_null($datetime):
+                return $carbonize->now($timezone);
+            case $datetime instanceof Carbon:
+                return $carbonize->fromCarbon($datetime, $timezone);
+            case $datetime instanceof DateTimeInterface:
+                return $carbonize->fromDateTime($datetime, $timezone);
+            case $carbonize->isTimestamp($datetime):
+                return $carbonize->fromTimestamp($datetime, $timezone);
+            case $carbonize->isDatetimeString($datetime):
+                return $carbonize->fromDatetimeString($datetime, $timezone);
+            default:
+                throw new InvalidArgumentException(
+                    "That is not a date time of any sort that I can deal with"
+                );
+        }
+    }
+
+    /**
+     * Check if the variable might be a timestamp.
+     *
+     * @param mixed $datetime
+     * @return bool
+     */
+    public function isTimestamp($datetime)
+    {
+        return is_numeric($datetime) && (string) (int) $datetime === (string) $datetime;
+    }
+
+    /**
+     * Check if the variable can be parsed as a datetime string.
+     *
+     * @param mixed $datetime
+     * @return bool
+     */
+    public function isDatetimeString($datetime)
+    {
+        return is_string($datetime) && strtotime($datetime) !== false;
+    }
+
+    /**
+     * Get an instance of Carbon for the current time.
+     *
+     * @param DateTimeZone|string|null $timezone
+     * @return Carbon
+     */
+    public function now($timezone)
+    {
+        return new Carbon(null, $timezone);
+    }
+
+    /**
+     * Get a new Carbon instance from an existing one.
+     *
+     * @param Carbon $carbon
+     * @param DateTimeZone|string|null $timezone
+     * @return Carbon
+     */
+    public function fromCarbon(Carbon $carbon, $timezone)
+    {
+        return $this->withTimezone(clone $carbon, $timezone);
+    }
+
+    /**
+     * Get a new Carbon instance from a DateTimeInterface instance.
+     *
+     * @param DateTimeInterface $datetime
+     * @param DateTimeZone|string|null $timezone
+     * @return Carbon
+     */
+    public function fromDateTime(DateTimeInterface $datetime, $timezone)
+    {
+        return $this->withTimezone(
+            new Carbon(
+                $datetime->format('Y-m-d H:i:s.u'),
+                $datetime->getTimezone()
+            ),
+            $timezone
+        );
+    }
+
+    /**
+     * Get a new Carbon instance from an integer or string timestamp.
+     *
+     * @param int|string $timestamp
+     * @param DateTimeZone|string|null $timezone
+     * @return Carbon
+     */
+    public function fromTimestamp($timestamp, $timezone)
+    {
+        return (new Carbon(null, $timezone))->setTimestamp($timestamp);
+    }
+
+    /**
+     * Get a new Carbon instance from a datetime string.
+     *
+     * @param string $datetime
+     * @param DateTimeZone|string|null $timezone
+     * @return Carbon
+     */
+    public function fromDatetimeString($datetime, $timezone)
+    {
+        return $this->withTimezone(
+            new Carbon($datetime, $timezone),
+            $timezone
+        );
+    }
+
+    /**
+     * Set the timezone if one was supplied.
+     *
+     * @param Carbon $carbon
+     * @param DateTimeZone|string|null $timezone
+     * @return Carbon
+     */
+    private function withTimezone(Carbon $carbon, $timezone)
+    {
+        return is_null($timezone) ? $carbon : $carbon->setTimezone($timezone);
+    }
+}
--- src/functions.php
+++ src/functions.php
@@ -1,47 +1,29 @@
 <?php
 
-use Carbon\Carbon;
+use Carbonize\Carbonize;
 
 if (! function_exists('carbonize')) {
     /**
      * @param mixed $datetime
-     * @param string|null $tz
-     * @return Carbon
+     * @param DateTimeZone|string|null $timezone
+     * @return Carbon\Carbon
      * @throws InvalidArgumentException
      */
-    function carbonize($datetime = null, $tz = null)
+    function carbonize($datetime = null, $timezone = null)
     {
-        switch (true) {
-            case is_null($datetime):
-                return new Carbon(null, $tz);
-            case $datetime instanceof Carbon:
-                $dt = clone $datetime;
-                return is_null($tz) ? $dt : $dt->setTimezone($tz);
-            case $datetime instanceof DateTimeInterface:
-                $dt = new Carbon($datetime->format('Y-m-d H:i:s.u'), $datetime->getTimezone());
-                return is_null($tz) ? $dt : $dt->setTimezone($tz);
-            case is_numeric($datetime) && (string) (int) $datetime === (string) $datetime:
-                return Carbon::createFromTimestamp((int) $datetime, $tz);
-            case is_string($datetime) && strtotime($datetime) !== false:
-                $dt = new Carbon($datetime, $tz);
-                return is_null($tz) ? $dt : $dt->setTimezone($tz);
-            default:
-                throw new InvalidArgumentException(
-                    "That is not a date time of any sort that I can deal with"
-                );
-        }
+        return Carbonize::toCarbon($datetime, $timezone);
     }
 }
 
 if (! function_exists('carbon')) {
     /**
      * @param mixed $datetime
-     * @param string|null $tz
-     * @return Carbon
+     * @param DateTimeZone|string|null $timezone
+     * @return Carbon\Carbon
      * @throws InvalidArgumentException
      */
-    function carbon($datetime = null, $tz = null)
+    function carbon($datetime = null, $timezone = null)
     {
-        return carbonize($datetime, $tz);
+        return Carbonize::toCarbon($datetime, $timezone);
     }
 }
