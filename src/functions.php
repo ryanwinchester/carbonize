b776a5c8b84ab88ac9be739179eb876674299f2d
Ryan Winchester <code@ryanwinchester.ca>
2017-10-14 22:51:53 -0700

    fix broken link

---
+++ src/functions.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+
+    Hi, I've gone and moved stuff around.
+
+    - https://github.com/ryanwinchester/carbonize/blob/master/src/helpers.php
+    - https://github.com/ryanwinchester/carbonize/blob/master/src/Carbonize.php
+
+    Cheers （ ^_^）o自自o（^_^ ）
+
+ */
